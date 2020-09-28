755b373227d7f130254ef74291705dafff6782a0
Allen Li <allen.li@maiscrm.com>
2020-09-28 10:08:40 +0800

    layouts/layout: add config provider

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,69 +1,57 @@
 <template>
-  <div
-    class="theme-container"
-    :class="pageClasses"
-  >
-    <Navbar
-      v-if="shouldShowNavbar"
-    />
-
-    <!-- <div
-      class="sidebar-mask"
-      @click="toggleSidebar(false)"
-    /> -->
-
-    <Sidebar
-      :items="sidebarItems"
+  <a-config-provider :locale="locale">
+    <div
+      class="theme-container"
+      :class="pageClasses"
     >
-      <template #top>
-        <slot name="sidebar-top" />
-      </template>
-      <template #bottom>
-        <slot name="sidebar-bottom" />
-      </template>
-    </Sidebar>
-
-    <Home v-if="$page.frontmatter.home" />
-
-    <Page
-      v-else
-      :sidebar-items="sidebarItems"
-    >
-      <template #top>
-        <slot name="page-top" />
-      </template>
-      <template #bottom>
-        <slot name="page-bottom" />
-      </template>
-    </Page>
-
-    <a-back-top v-if="$themeConfig.backToTop"/>
-  </div>
+      <Navbar v-if="shouldShowNavbar" />
+      <Sidebar :items="sidebarItems">
+        <template #top>
+          <slot name="sidebar-top" />
+        </template>
+        <template #bottom>
+          <slot name="sidebar-bottom" />
+        </template>
+      </Sidebar>
+      
+        <Home v-if="$page.frontmatter.home" />
+        <Page v-else :sidebar-items="sidebarItems">
+          <template #top>
+            <slot name="page-top" />
+          </template>
+          <template #bottom>
+            <slot name="page-bottom" />
+          </template>
+        </Page>
+      <a-back-top v-if="$themeConfig.backToTop"/>
+    </div>
+  </a-config-provider>
 </template>
 
 <script>
+import { ConfigProvider } from 'ant-design-vue'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
 import Home from '@theme/components/Home.vue'
 import Navbar from '@theme/components/Navbar.vue'
 import Page from '@theme/components/Page.vue'
 import Sidebar from '@theme/components/Sidebar.vue'
+
 import { resolveSidebarItems } from '../util'
 
 export default {
   name: 'Layout',
-
   components: {
     Home,
     Page,
     Sidebar,
-    Navbar
+    Navbar,
+    ConfigProvider
   },
-
-  // data () {
-  //   return {
-  //     isSidebarOpen: false
-  //   }
-  // },
-
+  data() {
+    return {
+      locale: zhCN
+    }
+  },
   computed: {
     shouldShowNavbar () {
       const { themeConfig } = this.$site
@@ -81,7 +69,6 @@ export default {
         || this.$themeLocaleConfig.nav
       )
     },
-
     shouldShowSidebar () {
       const { frontmatter } = this.$page
       return (
@@ -90,7 +77,6 @@ export default {
         && this.sidebarItems.length
       )
     },
-
     sidebarItems () {
       return resolveSidebarItems(
         this.$page,
@@ -99,51 +85,16 @@ export default {
         this.$localePath
       )
     },
-
     pageClasses () {
       const userPageClass = this.$page.frontmatter.pageClass
       return [
         {
           'no-navbar': !this.shouldShowNavbar,
-          // 'sidebar-open': this.isSidebarOpen,
           'no-sidebar': !this.shouldShowSidebar
         },
         userPageClass
       ]
     }
   },
-
-  // mounted () {
-  //   this.$router.afterEach(() => {
-  //     this.isSidebarOpen = false
-  //   })
-  // },
-
-  // methods: {
-    // toggleSidebar (to) {
-    //   this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-    //   this.$emit('toggle-sidebar', this.isSidebarOpen)
-    // },
-
-    // side swipe
-    // onTouchStart (e) {
-      // this.touchStart = {
-      //   x: e.changedTouches[0].clientX,
-      //   y: e.changedTouches[0].clientY
-      // }
-    // },
-
-    // onTouchEnd (e) {
-      // const dx = e.changedTouches[0].clientX - this.touchStart.x
-      // const dy = e.changedTouches[0].clientY - this.touchStart.y
-      // if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-      //   if (dx > 0 && this.touchStart.x <= 80) {
-      //     this.toggleSidebar(true)
-      //   } else {
-      //     this.toggleSidebar(false)
-      //   }
-      // }
-    // }
-  // }
 }
 </script>
